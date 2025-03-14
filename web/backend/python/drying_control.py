4d13d7aca805aed40e4db10247e1edf27eccfaba
Rlzee <romainprovencel@icloud.com>
2025-03-14 10:28:24 +0100

    Add GPIO control for drying process

--- web/backend/python/drying_control.py
+++ web/backend/python/drying_control.py
@@ -1,1 +1,22 @@
-# code here
+import RPi.GPIO as GPIO
+import time
+
+RELAY_PIN = 17 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+def start_drying():
+    try:
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        print("Séchage démarré : Brûleur allumé")
+        
+    except KeyboardInterrupt:
+        GPIO.output(RELAY_PIN, GPIO.LOW)
+        print("Séchage arrêté : Brûleur éteint")
+        GPIO.cleanup()
+
+def stop_drying():
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    print("Séchage arrêté : Brûleur éteint")
+    GPIO.cleanup()
