e578f9885a3e3a0a2b7b1eca437ce7dd851b566e
Rlzee <romainprovencel@icloud.com>
2025-03-14 10:23:41 +0100

    Implement start and stop drying API endpoints

--- web/backend/php/api/start_drying.php
+++ web/backend/php/api/start_drying.php
@@ -1,1 +1,10 @@
-<!-- code here -->
+<?php
+include './classes/dryingControl.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $dryingControl = new DryingControl();
+    $response = $dryingControl->startDrying();
+    
+    echo $response;
+}
+?>
