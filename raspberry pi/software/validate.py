2de84196440c8546db0404cdf58f4b711d1a8d19
fateh kabbani <fatihkabbani223@gmail.com>
2025-03-20 14:47:03 +0100

    Add GPIO control script for LED and button interaction with event detection

---
+++ raspberry pi/software/validate.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 17
+BtnPin = 18
+
+Led_status = 1
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.output(LedPin, GPIO.HIGH)
+
+def swLed(ev=None):
+    global Led_status
+    Led_status = not Led_status
+    GPIO.output(LedPin, Led_status)
+    if Led_status == 1:
+        print ("led off...")
+    else:
+        print ("...led on")
+
+def loop():
+    GPIO.add_event_detect(BtnPin, GPIO.FALLING, callback=swLed, bouncetime=200)
+    while True:
+        time.sleep(1)
+
+def destroy():
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
