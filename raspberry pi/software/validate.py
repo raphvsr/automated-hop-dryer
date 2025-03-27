9dfa6a7d8bda0f7f7998ddb6f37f420598f4b8ae
fateh kabbani <fatihkabbani223@gmail.com>
2025-03-27 11:41:53 +0100

    .

--- raspberry pi/software/validate.py
+++ raspberry pi/software/validate.py
@@ -17,10 +17,8 @@ def swLed(ev=None):
     Led_status = not Led_status
     GPIO.output(LedPin, Led_status)
     if Led_status == 1:
-        print ("led off...")
-    else:
-        print ("...led on")
-
+        # valider et ajouter dans la base de donner et dans le ficher csv
+        print('test')
 def loop():
     GPIO.add_event_detect(BtnPin, GPIO.FALLING, callback=swLed, bouncetime=200)
     while True:
