a3b12318f62c68f54ee9ec64c2dcb4caf77c0a3e
fateh kabbani <fatihkabbani223@gmail.com>
2025-04-01 13:35:37 +0200

    Implement user editing functionality: add modal for editing user details and AJAX request for updates

--- raspberry pi/web/admin/src/js/users.js
+++ raspberry pi/web/admin/src/js/users.js
@@ -14,3 +14,50 @@ function deleteUser(id) {
       });
   }
 }
+
+$('.btn-edit').on('click', function () {
+  const userId = $(this).data('id');
+  const username = $(this).closest('tr').find('td:first').text();
+  const role = $(this).closest('tr').find('.role-badge').hasClass('admin') ? '1' : '0';
+  $('#userModal').show();
+  $('#userId').val(userId);
+  $('#username').val(username);
+  $('#role').val(role);
+
+});
+
+$('.close, .btn-cancel').on('click', function () {
+  $('#userModal').hide();
+});
+
+$('#save').on('click', function (e) {
+  console.log('save');
+  const userId = $('#userId').val();
+  const username = $('#username').val();
+  let password = $('#password').val();
+  const role = $('#role').val();
+
+  if(password == ''){
+    password = null;
+  }
+
+  $.post('../backend/php/api/users-update.php',
+    {
+      username: username,
+      role: role,
+      password: password || "",
+      id: userId
+    }
+  )
+    .done(function (response) {
+      const data = JSON.parse(response);
+      if (data.status === 'success') {
+          location.reload();
+      } else {
+        alert(data.message);
+      }
+    })
+    .fail(function () {
+      alert('Erreur lors de la requête AJAX.');
+    });
+});
