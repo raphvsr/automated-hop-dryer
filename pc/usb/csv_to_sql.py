cd4d171715c7e1b29846325009b40eebc492367c
p0ubelle <raphael.vasseur26@gmail.com>
2025-04-03 14:53:44 +0200

    .

--- pc/usb/csv_to_sql.py
+++ pc/usb/csv_to_sql.py
@@ -1,6 +1,20 @@
 
 import mysql.connector
 
+
+def get_csv_data():
+    with open("data.csv") as file:
+        for i, line in enumerate(file):
+            if i == 0:
+                continue
+            values = line.split(",")
+            if len(values) != 8:
+                continue
+            date, heure, etage4, etage3, etage2, etage1, temps_bruleur, etat_bruleur = values
+            print(heure)
+
+get_csv_data()
+
 db = mysql.connector.connect(
     host="localhost:3306",
     user="root",
@@ -8,14 +22,13 @@ db = mysql.connector.connect(
     database="DB_PC"
 )
 
-
 cursor = db.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    age INT
-)
+    ALTER TABLE burner_status
+    MODIFY COLUMN status ENUM('on', 'off') NOT NULL;
 """)
+
+db.commit()
+cursor.close()
+db.close()
