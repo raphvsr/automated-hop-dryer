aaef99d3179a2a439a2230d801097f8abb88eee0
Poubelle26 <69398423+p0ubelle@users.noreply.github.com>
2025-04-07 00:07:48 +0200

    t

--- pc/usb/csv_to_sql.py
+++ pc/usb/csv_to_sql.py
@@ -3,32 +3,34 @@ import mysql.connector
 
 
 def get_csv_data():
+    rows = []
     with open("data.csv") as file:
         for i, line in enumerate(file):
             if i == 0:
                 continue
             values = line.split(",")
-            if len(values) != 8:
+            if len(values) != 7:
                 continue
-            date, heure, etage4, etage3, etage2, etage1, temps_bruleur, etat_bruleur = values
-            print(heure)
+            date, etage4, etage3, etage2, etage1, temps_bruleur, etat_bruleur = values
+            status = "true" if etat_bruleur == "true" else "off"
+            campaign_id = 1
+            rows.append((campaign_id, status, date))
+
 
 get_csv_data()
 
-db = mysql.connector.connect(
-    host="localhost:3306",
-    user="root",
-    password="",
-    database="DB_PC"
-)
 
-cursor = db.cursor()
 
-cursor.execute("""
-    ALTER TABLE burner_status
-    MODIFY COLUMN status ENUM('on', 'off') NOT NULL;
-""")
+# db = mysql.connector.connect(
+#     host="localhost:3306",
+#     user="root",
+#     password="",
+#     database="DB_PC"
+# )
 
-db.commit()
-cursor.close()
-db.close()
+# cursor = db.cursor()
+
+
+# db.commit()
+# cursor.close()
+# db.close()
